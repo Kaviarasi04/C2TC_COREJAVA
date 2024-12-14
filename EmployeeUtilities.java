7175c53efe3c0d906da1ba47f7e46d0f18013dc5
Kaviarasi04 <kaviarasivelayutham@gmail.com>
2024-12-14 21:24:46 +0530

    Update and rename EmployeeHelper.java to EmployeeUtilities.java

---
+++ EmployeeUtilities.java
@@ -0,0 +1,19 @@
+package com.kaviarasi.assignment;
+import com.kaviarasi.assignment.Employee;
+import com.kaviarasi.assignment.Manager;
+import com.kaviarasi.assignment.Developer;
+public class EmployeeUtilities {
+public static void printEmployeeDetails(Employee employee) {
+System.out.println("Employee Name: " + employee.getName());
+System.out.println("Employee ID: " + employee.getEmployeeId());
+System.out.println("Salary: " + employee.getSalary());
+if (employee instanceof Manager) {
+Manager manager = (Manager) employee;
+System.out.println("Department: " + manager.getDepartment());
+} else if (employee instanceof Developer) {
+Developer developer = (Developer) employee;
+System.out.println("Programming Language: " + developer.getProgrammingLanguage());
+}
+System.out.println();
+}
+}
